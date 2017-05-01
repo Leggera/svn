314bffab5eff67ef3750820bd3579af5a5984959
Leggera <lana.krlv1995@gmail.com>
2017-05-01 19:33:31 +0300

    Update 20ng_combine_all.sh

--- 20ng_combine_all.sh
+++ 20ng_combine_all.sh
@@ -33,7 +33,7 @@ for model in "${default_models[@]}"; do
       d2v_t="$time_fold""time_""$d2v_out"
       echo "2"
       echo ${n_p[@]}
-      (time (python3 run_doc2vec_20ng.py -output "$space_fold""$d2v_out" $model  ${n_p[@]} >> "$d2v_t")) &>> "$d2v_t" &
+      #(time (python3 run_doc2vec_20ng.py -output "$space_fold""$d2v_out" $model  ${n_p[@]} >> "$d2v_t")) &>> "$d2v_t" &
     done
     for alpha in "${alphas[@]}"; do
         delete=("-alpha 0.05")
@@ -43,7 +43,7 @@ for model in "${default_models[@]}"; do
         d2v_t="$time_fold""time_""$d2v_out"
         echo "3"
         echo ${n_p[@]}
-        (time (python3 run_doc2vec_20ng.py -output "$space_fold""$d2v_out" $model  ${n_p[@]} >> "$d2v_t")) &>> "$d2v_t" &
+        #(time (python3 run_doc2vec_20ng.py -output "$space_fold""$d2v_out" $model  ${n_p[@]} >> "$d2v_t")) &>> "$d2v_t" &
     done
     for size in "${sizes[@]}"; do
         delete=("-size 150")
@@ -53,7 +53,7 @@ for model in "${default_models[@]}"; do
         d2v_t="$time_fold""time_""$d2v_out"
         echo "4"
         echo ${n_p[@]}
-        (time (python3 run_doc2vec_20ng.py -output "$space_fold""$d2v_out" $model  ${n_p[@]} >> "$d2v_t")) &>> "$d2v_t" &
+        #(time (python3 run_doc2vec_20ng.py -output "$space_fold""$d2v_out" $model  ${n_p[@]} >> "$d2v_t")) &>> "$d2v_t" &
     done
   done
 done
