e07cdb37c2927e50db82ee94809da9523d42f7a2
Leggera <lana.krlv1995@gmail.com>
2017-05-01 19:37:47 +0300

    Update 20ng_combine_all.sh

--- 20ng_combine_all.sh
+++ 20ng_combine_all.sh
@@ -26,6 +26,8 @@ for model in "${default_models[@]}"; do
     echo "1"
     echo ${d_p[@]}
     for window in "${windows[@]}"; do
+      echo "2"
+      echo ${d_p[@]}
       delete=("-window 10")
       n_p=${d_p[@]/$delete}
       echo ${n_p[@]}
@@ -33,7 +35,7 @@ for model in "${default_models[@]}"; do
       echo ${n_p[@]}
       d2v_out="doc2vec ""$model""$n_p"".txt"
       d2v_t="$time_fold""time_""$d2v_out"
-      echo "2"
+      
       echo ${n_p[@]}
       #(time (python3 run_doc2vec_20ng.py -output "$space_fold""$d2v_out" $model  ${n_p[@]} >> "$d2v_t")) &>> "$d2v_t" &
     done
