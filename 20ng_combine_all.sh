fece7dd5175985ca5bbdde23ca73114e027df1ab
Leggera <lana.krlv1995@gmail.com>
2017-05-01 18:58:18 +0300

    Update 20ng_combine_all.sh

--- 20ng_combine_all.sh
+++ 20ng_combine_all.sh
@@ -45,5 +45,5 @@ for model in "${default_models[@]}"; do
         d2v_t="$time_fold""time_""$d2v_out"
         (time (python3 run_doc2vec_20ng.py -output "$space_fold""$d2v_out" $model $d_p >> "$d2v_t")) &>> "$d2v_t" &
     done
-  
+  done
 done
