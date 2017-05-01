6bf15fd97956be92cdfffcc5722d9200da4e22f8
Leggera <lana.krlv1995@gmail.com>
2017-05-01 16:58:28 +0300

    Update assemble.py

--- assemble.py
+++ assemble.py
@@ -2,6 +2,7 @@ import pandas as pd
 import os
 res = pd.DataFrame()
 for dir in os.listdir('runs_IMDB'):
+  print (dir)
   for csvfile in os.listdir(dir):
     df = pd.open_csv(csvfile)
     res = res + df
