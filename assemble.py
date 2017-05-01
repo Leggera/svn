784d53ae8e3b9b8e3a5c54077aa3985e69df8fbc
Leggera <lana.krlv1995@gmail.com>
2017-05-01 17:00:29 +0300

    Update assemble.py

--- assemble.py
+++ assemble.py
@@ -2,9 +2,8 @@ import pandas as pd
 import os
 res = pd.DataFrame()
 for dir in os.listdir('runs_IMDB'):
-  print (dir)
-  for csvfile in os.listdir(dir):
-    df = pd.open_csv(csvfile)
+  for csvfile in os.listdir(dir+"/"):
+    df = pd.open_csv(dir + "/" + csvfile)
     res = res + df
 res.to_csv('IMDB_.csv')
   
