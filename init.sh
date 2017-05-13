0fc8001cee2371286c7e922d267217c5e0c4ba4d
Leggera <lana.krlv1995@gmail.com>
2017-05-13 22:29:39 +0300

    Create init.sh

---
+++ init.sh
@@ -0,0 +1,16 @@
+sudo apt-get install htop
+sudo apt-get update
+#!/usr/bin/env bash
+export LC_ALL="en_US.UTF-8"
+export LC_CTYPE="en_US.UTF-8"
+sudo dpkg-reconfigure locales
+sudo apt-get install htop g++ git aptitude cmake python3
+sudo apt-get install python3-pip
+sudo apt-get install python3-numpy
+sudo apt-get install python3-scipy
+sudo  pip3 install sklearn
+sudo  pip3 install pandas
+sudo -E add-apt-repository ppa:nilarimogard/webupd8
+sudo apt-get update
+sudo apt-get install grive
+sudo apt-get install p7zip-full
