314ba25d09e92b68159e6e53e7723be401e770eb
Leggera <lana.krlv1995@gmail.com>
2017-05-14 10:18:22 +0300

    Update run_doc2vecs_20ng.sh

--- run_doc2vecs_20ng.sh
+++ run_doc2vecs_20ng.sh
@@ -36,7 +36,6 @@ for model in "${default_models[@]}"; do
 	    d2v_t="$time_fold""time_""$d2v_out"
 	    (time (python3 run_doc2vec_20ng.py  -output "$space_fold""$d2v_out" $alpha $model $d_p >> "$d2v_t")) &>> "$d2v_t" &
 	done
-	wait
 	for window in "${windows[@]}"; do
 	    delete=("-window 10")
 	    d_p=${default_parameters[@]/$delete}
@@ -55,7 +54,6 @@ for model in "${default_models[@]}"; do
 	d2v_t="$time_fold""time_""$d2v_out"
 	(time (python3 run_doc2vec_20ng.py -output "$space_fold""$d2v_out" $negative $model $d_p >> "$d2v_t")) &>> "$d2v_t" &
     done
-    wait
 done
 wait
 for model in "${models[@]}"; do
