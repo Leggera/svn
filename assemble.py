309a8fd652d0f5ea9615a9b87305c237421cce7a
Leggera <lana.krlv1995@gmail.com>
2017-05-15 07:44:18 +0300

    Update assemble.py

--- assemble.py
+++ assemble.py
@@ -1,9 +1,20 @@
 import pandas as pd
 import os
-res = pd.DataFrame()
-for dir in os.listdir('runs_20ng'):
-  for csvfile in os.listdir('runs_20ng/' + dir):
-    df = pd.read_csv('runs_20ng/' + dir + "/" + csvfile)
-    res = pd.concat([res, df])
-res.to_csv('20ng_.csv')
-  
+import sys
+
+dir_dict = dict()
+dir_dict['20ng'] = 'runs_20ng'
+dir_dict['IMDB'] = 'runs_IMDB'
+
+def main():
+    # print command line arguments
+    arg = sys.argv[1]
+    res = pd.DataFrame()
+    for dir in os.listdir(dir_dict[arg]):
+      for csvfile in os.listdir(dir_dict[arg] + '/' + dir):
+        df = pd.read_csv(dir_dict[arg] + '/' + dir + "/" + csvfile)
+        res = pd.concat([res, df])
+    res.to_csv(arg + 'assembled.csv')
+
+if __name__ == "__main__":
+    main()
