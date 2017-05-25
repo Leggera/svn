90b5a44a22ea1eee089d39c64378f7288d1d4e05
Leggera <lana.krlv1995@gmail.com>
2017-05-26 00:32:30 +0300

    Update run_doc2vecs_alpha_IMDB.sh

--- run_doc2vecs_alpha_IMDB.sh
+++ run_doc2vecs_alpha_IMDB.sh
@@ -52,6 +52,7 @@ for iter in "${iters[@]}";do
             d2v_t="$time_fold""time_""$d2v_out"
             (time (python3 run_doc2vec_proper.py  -output "$space_fold""$d2v_out" $iter $m_c $alpha $model $d_p >> "$d2v_t")) &>> "$d2v_t" &
         done
+	wait
   done
 done
 wait
