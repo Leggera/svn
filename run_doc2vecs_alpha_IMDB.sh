4986b07311b717f3887cd31baa89f92c15100633
Leggera <lana.krlv1995@gmail.com>
2017-05-26 00:48:08 +0300

    Update run_doc2vecs_alpha_IMDB.sh

--- run_doc2vecs_alpha_IMDB.sh
+++ run_doc2vecs_alpha_IMDB.sh
@@ -34,6 +34,7 @@ alphas=('-alpha 0.01' '-alpha 0.02' '-alpha 0.03' '-alpha 0.04' '-alpha 0.05' '-
 '-alpha 0.08' '-alpha 0.09' '-alpha 0.1' '-alpha 0.15' '-alpha 0.2')
 default_parameters=('-size 150 -alpha 0.05 -window 10 -negative 25 -threads 1 -train alldata-id.txt')
 default_models=('-cbow 0 -sample 1e-2' '-cbow 1 -sample 1e-4')
+iters=('-iter 25')
 min_count=('-min_count 1')
 
 mkdir time_d2v
