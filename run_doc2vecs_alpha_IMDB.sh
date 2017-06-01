746b2c1a5c6aef8595e6e12501c392a92c11bf81
Leggera <lana.krlv1995@gmail.com>
2017-06-01 18:49:03 +0300

    Update run_doc2vecs_alpha_IMDB.sh

--- run_doc2vecs_alpha_IMDB.sh
+++ run_doc2vecs_alpha_IMDB.sh
@@ -33,7 +33,10 @@ awk 'BEGIN{a=0;}{print "_*" a " " $0; a++;}' < alldata.txt > alldata-id.txt
 #alphas1=('-alpha 0.01' '-alpha 0.02' '-alpha 0.03' '-alpha 0.04' '-alpha 0.05' '-alpha 0.06')
 #alphas2=('-alpha 0.07' '-alpha 0.08' '-alpha 0.09' '-alpha 0.1' '-alpha 0.15' '-alpha 0.2')
 default_parameters=('-size 150 -window 10 -negative 25 -threads 1 -train alldata-id.txt')
-default_models=('-cbow 1 -sample 1e-4 -alpha 0.2' '-cbow 0 -sample 1e-2 -alpha 0.05' '-cbow 0 -sample 1e-2 -alpha 0.09''-cbow 0 -sample 1e-2 -alpha 0.02')
+default_models=('-cbow 1 -sample 1e-4 -alpha 0.07' '-cbow 1 -sample 1e-4 -alpha 0.2' '-cbow 1 -sample 1e-4 -alpha 0.6'
+'-cbow 0 -sample 1e-2 -alpha 0.017' '-cbow 0 -sample 1e-2 -alpha 0.05' '-cbow 0 -sample 1e-2 -alpha 0.15'
+'-cbow 0 -sample 1e-2 -alpha 0.03' '-cbow 0 -sample 1e-2 -alpha 0.09' '-cbow 0 -sample 1e-2 -alpha 0.27'
+'-cbow 0 -sample 1e-2 -alpha 0.007' '-cbow 0 -sample 1e-2 -alpha 0.02' '-cbow 0 -sample 1e-2 -alpha 0.06')
 iters=('-iter 25')
 min_count=('-min_count 1')
 
