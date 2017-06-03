54a8317c594b6ccf3cf9c4731a8fad4d318afec2
Leggera <lana.krlv1995@gmail.com>
2017-06-03 12:06:33 +0300

    Update run_word2vecs_IMDB.sh

--- run_word2vecs_IMDB.sh
+++ run_word2vecs_IMDB.sh
@@ -36,7 +36,7 @@ awk 'BEGIN{a=0;}{print "_*" a " " $0; a++;}' < alldata.txt > alldata-id.txt
 
 sizes=('-size 75')
 alphas=('-alpha 0.025' '-alpha 0.1')
-windows=('-window 5' '-window 20')
+windows=('-window 5')
 negatives=('-negative 12' '-negative 50')
 iters=('-iter 25')
 
